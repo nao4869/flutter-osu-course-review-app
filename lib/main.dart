b9366fa8b032fa0ac218c3b9a8afa9b055ac816d
Rails Course <muraokan@oregonstate.edu>
2020-01-23 15:51:54 +0900

    main, pubspec.yaml replaced with providers one

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:osu_course_review/data/sample_courses_data.dart';
-//import './screens/list_courses_screen.dart';
+import 'package:provider/provider.dart';
 
 import './screens/course_detail_screen.dart';
-import './screens/tabs_screen.dart';
+import './screens/list_courses_screen.dart';
 import './screens/create_course_screen.dart';
-import './screens/create_review_screen.dart';
 
-import './models/course.dart';
-import './models/review.dart';
+import './models/courses_provider.dart';
+import './models/reviews_provider.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyApp> {
-  List<Course> _allCourses= SAMPLE_COURSES.toList();
-  List<Review> _allReviews= SAMPLE_REVIEWS.toList();
-  
-
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Courses(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Reviews(),
+        ),
+      ],
+    child: MaterialApp(
       title: 'OSU Course Review',
       theme: ThemeData(
         primarySwatch: Colors.purple,
@@ -44,13 +48,13 @@ class _MyHomePageState extends State<MyApp> {
               fontWeight: FontWeight.bold,
             )),
       ),
+      home: ListCoursesScreen(),
       // Lists of available routes in the application
-      routes: {
-        '/': (context) => TabScreen(), // home route
-        CourseDetailScreen.routeName: (context) => CourseDetailScreen(_allCourses, _allReviews),
+      routes: { 
+        CourseDetailScreen.routeName: (context) => CourseDetailScreen(),
         CreateCourseScreen.routeName: (context) => CreateCourseScreen(),
-        CreateReviewScreen.routeName: (context) => CreateReviewScreen(),
-      },
+
+      }),
     );
   }
 }
