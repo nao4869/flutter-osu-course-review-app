0d18d50e1d19882186801692a95df3831edd1867
Rails Course <muraokan@oregonstate.edu>
2020-01-25 12:07:00 +0900

    main.dart file configured to retrieve course id when calling Reviews provider

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,9 @@ import './screens/course_detail_screen.dart';
 import './screens/list_courses_screen.dart';
 import './screens/create_course_screen.dart';
 
+import './models/course.dart';
 import './models/courses_provider.dart';
 import './models/reviews_provider.dart';
-
 
 void main() => runApp(MyApp());
 
@@ -24,37 +24,37 @@ class _MyAppState extends State<MyApp> {
         ChangeNotifierProvider.value(
           value: Courses(),
         ),
-        ChangeNotifierProvider.value(
-          value: Reviews(),
+        ChangeNotifierProxyProvider<Course, Reviews>(
+          builder: (ctx, course, previousReviews) => Reviews(course.id,
+              previousReviews == null ? [] : previousReviews.reviews),
         ),
       ],
-    child: MaterialApp(
-      title: 'OSU Course Review',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.amber,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: 'Raleway',
-        textTheme: ThemeData.light().textTheme.copyWith(
-            body1: TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
-            ),
-            body2: TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
-            ),
-            title: TextStyle(
-              fontSize: 20,
-              fontFamily: 'RobotoCondensed',
-              fontWeight: FontWeight.bold,
-            )),
-      ),
-      home: ListCoursesScreen(),
-      // Lists of available routes in the application
-      routes: { 
-        CourseDetailScreen.routeName: (context) => CourseDetailScreen(),
-        CreateCourseScreen.routeName: (context) => CreateCourseScreen(),
-
-      }),
+      child: MaterialApp(
+          title: 'OSU Course Review',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.amber,
+            canvasColor: Color.fromRGBO(255, 254, 229, 1),
+            fontFamily: 'Raleway',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                body1: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                body2: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold,
+                )),
+          ),
+          home: ListCoursesScreen(),
+          // Lists of available routes in the application
+          routes: {
+            CourseDetailScreen.routeName: (context) => CourseDetailScreen(),
+            CreateCourseScreen.routeName: (context) => CreateCourseScreen(),
+          }),
     );
   }
 }
