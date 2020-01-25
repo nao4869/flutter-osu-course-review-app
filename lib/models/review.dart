efba4d0799f77f1db4f5f180126b3bd262ce9ca9
Rails Course <muraokan@oregonstate.edu>
2020-01-25 12:00:01 +0900

    required parameter courseId has removed from review class

--- lib/models/review.dart
+++ lib/models/review.dart
@@ -3,12 +3,10 @@ import 'package:flutter/foundation.dart';
 
 class Review with ChangeNotifier {
   final String id;
-  final String courseId;
   final String reviewsContent;
 
   Review({
     @required this.id,
-    @required this.courseId,
     @required this.reviewsContent,
   });
 }
