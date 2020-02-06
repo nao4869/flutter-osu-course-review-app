247224e4c9688eb09b83abe9f94e6c3062dedb09
Rails Course <muraokan@oregonstate.edu>
2020-02-06 13:03:00 +0900

    logo added to major model

--- lib/models/major.dart
+++ lib/models/major.dart
@@ -5,12 +5,14 @@ class Major with ChangeNotifier {
   final String id;
   final String majorName;
   final String institutionName;
+  final String logo;
   final Color color;
 
   Major({
     @required this.id,
     @required this.majorName,
     @required this.institutionName,
+    @required this.logo,
     this.color = Colors.orange, // default color
   });
 }
