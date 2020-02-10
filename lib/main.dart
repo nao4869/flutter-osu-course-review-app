43dc6aee71c8bc51a92b7ba0e658361a9032333a
zhirencungang@gmail.com <muraokan@oregonstate.edu>
2020-02-10 22:35:09 +0900

    can now dismiss the keyboard

--- lib/main.dart
+++ lib/main.dart
@@ -50,37 +50,45 @@ class _MyAppState extends State<MyApp> {
               previousReviews == null ? [] : previousReviews.reviews),
         ),
       ],
-      child: MaterialApp(
-          title: 'OSU Course Review',
-          theme: ThemeData(
-            primarySwatch: Colors.red,
-            accentColor: Colors.amberAccent,
-            canvasColor: Color.fromRGBO(250, 245, 240, 1),
-            fontFamily: 'Raleway',
-            textTheme: ThemeData.light().textTheme.copyWith(
-                  body1: TextStyle(
-                    color: Color.fromRGBO(20, 51, 51, 1),
+      child: GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: MaterialApp(
+            title: 'OSU Course Review',
+            theme: ThemeData(
+              primarySwatch: Colors.red,
+              accentColor: Colors.amberAccent,
+              canvasColor: Color.fromRGBO(250, 245, 240, 1),
+              fontFamily: 'Raleway',
+              textTheme: ThemeData.light().textTheme.copyWith(
+                    body1: TextStyle(
+                      color: Color.fromRGBO(20, 51, 51, 1),
+                    ),
+                    body2: TextStyle(
+                      color: Color.fromRGBO(20, 51, 51, 1),
+                    ),
+                    title: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'RobotoCondensed',
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  body2: TextStyle(
-                    color: Color.fromRGBO(20, 51, 51, 1),
-                  ),
-                  title: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'RobotoCondensed',
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-          ),
-          home: TabScreen(),
-          routes: {
-            CourseDetailScreen.routeName: (context) => CourseDetailScreen(),
-            CreateCourseScreen.routeName: (context) => CreateCourseScreen(),
-            CreateReviewScreen.routeName: (context) => CreateReviewScreen(),
-            ListInstitutionScreen.routeName: (context) =>
-                ListInstitutionScreen(),
-            ListMajorsScreen.routeName: (context) => ListMajorsScreen(),
-            ListCoursesScreen.routeName: (context) => ListCoursesScreen(),
-          }),
+            ),
+            home: TabScreen(),
+            routes: {
+              CourseDetailScreen.routeName: (context) => CourseDetailScreen(),
+              CreateCourseScreen.routeName: (context) => CreateCourseScreen(),
+              CreateReviewScreen.routeName: (context) => CreateReviewScreen(),
+              ListInstitutionScreen.routeName: (context) =>
+                  ListInstitutionScreen(),
+              ListMajorsScreen.routeName: (context) => ListMajorsScreen(),
+              ListCoursesScreen.routeName: (context) => ListCoursesScreen(),
+            }),
+      ),
     );
   }
 }
