4174b2fbde0e29d4145d3f9b54afdf28594aad58
naohito muraoka <muraokan@oregonstate.edu>
2020-02-20 23:18:48 +0900

    course list item widget refactored

--- lib/widgets/course_list_item.dart
+++ lib/widgets/course_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:osu_course_review/models/language.dart';
 import 'package:osu_course_review/screens/course_detail_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -11,9 +12,109 @@ class CourseListItem extends StatefulWidget {
 }
 
 class _CourseListItemState extends State<CourseListItem> {
+  Widget _displayCourseNameAndFavIcon(
+      BuildContext context, String courseName, Auth authData) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 10, 0, 3),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              courseName,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                fontFamily: 'Roboto',
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.clip,
+              softWrap: false,
+            ),
+          ),
+          Container(
+            child: Consumer<Course>(
+              builder: (ctx, course, child) => IconButton(
+                icon: Icon(
+                  course.isFavorite ? Icons.favorite : Icons.favorite_border,
+                  size: 30,
+                  color: course.isFavorite == true ? Colors.pink : Colors.grey,
+                ),
+                color: Theme.of(context).accentColor,
+                onPressed: () {
+                  course.toggleFavoriteStatus(authData.token, authData.userId);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _displayCourseSubItem(String text, String courseItem) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 0, 0, 3),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              text + courseItem,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontFamily: 'Roboto',
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.clip,
+              softWrap: false,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _displayRaisedButton(
+      BuildContext context, String courseId, String buttonText) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.fromLTRB(17, 0, 17, 0),
+      child: ButtonTheme(
+        minWidth: double.infinity,
+        height: 30.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CourseDetailScreen(),
+                settings: RouteSettings(arguments: courseId),
+              ),
+            );
+          },
+          child: Text(
+            buttonText,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          color: Theme.of(context).primaryColor,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // retrieving providers objects
+    const language = 'Language: ';
+    const prerequisite = 'Course Prerequisite: ';
+    const buttonText = 'Check all reviews';
     final course = Provider.of<Course>(context, listen: false);
     final authData = Provider.of<Auth>(context, listen: false);
 
@@ -29,119 +130,10 @@ class _CourseListItemState extends State<CourseListItem> {
         width: double.infinity,
         child: Column(
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 10, 0, 3),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      course.courseName,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        fontFamily: 'Roboto',
-                      ),
-                      maxLines: 1,
-                      overflow: TextOverflow.clip,
-                      softWrap: false,
-                    ),
-                  ),
-                  Container(
-                    child: Consumer<Course>(
-                      builder: (ctx, course, child) => IconButton(
-                        icon: Icon(
-                          course.isFavorite
-                              ? Icons.favorite
-                              : Icons.favorite_border,
-                          size: 30,
-                          color: course.isFavorite == true
-                              ? Colors.pink
-                              : Colors.grey,
-                        ),
-                        color: Theme.of(context).accentColor,
-                        onPressed: () {
-                          course.toggleFavoriteStatus(
-                              authData.token, authData.userId);
-                        },
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 0, 3),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      'Pre-requisite: ' + course.prerequisite,
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontFamily: 'Roboto',
-                      ),
-                      maxLines: 1,
-                      overflow: TextOverflow.clip,
-                      softWrap: false,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 0, 3),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      'Programming Language: ' + course.language,
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontFamily: 'Roboto',
-                      ),
-                      maxLines: 2,
-                      // overflow: TextOverflow.clip,
-                      // softWrap: false,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.fromLTRB(17, 0, 17, 0),
-              child: ButtonTheme(
-                minWidth: double.infinity,
-                height: 30.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: RaisedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CourseDetailScreen(),
-                        settings: RouteSettings(arguments: course.id),
-                      ),
-                    );
-                  },
-                  child: Text(
-                    "Check all reviews",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  color: Theme.of(context).primaryColor,
-                ),
-              ),
-            ),
+            _displayCourseNameAndFavIcon(context, course.courseName, authData),
+            _displayCourseSubItem(prerequisite, course.prerequisite),
+            _displayCourseSubItem(language, course.language),
+            _displayRaisedButton(context, course.id, buttonText),
           ],
         ),
       ),
