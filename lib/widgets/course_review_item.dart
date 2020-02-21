1102d0f6f235fc4fdabb258b399a101cc1109931
naohito muraoka <muraokan@oregonstate.edu>
2020-02-21 21:59:49 +0900

    course review items screen refactored as functions

--- lib/widgets/course_review_item.dart
+++ lib/widgets/course_review_item.dart
@@ -5,9 +5,54 @@ import '../models/star_display.dart';
 import '../models/review.dart';
 
 class CourseReviewItem extends StatelessWidget {
+  Widget _displayStarScore(int score) {
+    return Container(
+      alignment: Alignment.topLeft,
+      padding: EdgeInsets.fromLTRB(17, 10, 0, 0),
+      child: StarDisplayWidget(
+        value: score,
+        filledStar: Icon(Icons.star, color: Colors.amber, size: 20),
+        unfilledStar: Icon(Icons.star_border, color: Colors.grey),
+      ),
+    );
+  }
+
+  Widget _displayReviewsContent(String reviewContent) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 7,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 5, 15, 0),
+            child: Text(
+              reviewContent,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _displayDateTime(dynamic createdAt) {
+    return Container(
+      alignment: Alignment.bottomRight,
+      padding: EdgeInsets.fromLTRB(0, 10, 15, 5),
+      child: Text(
+        createdAt,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 15,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // retrieving providers objects
     final review = Provider.of<Review>(context, listen: false);
 
     return Container(
@@ -23,43 +68,9 @@ class CourseReviewItem extends StatelessWidget {
             width: double.infinity,
             child: Column(
               children: <Widget>[
-                Container(
-                  alignment: Alignment.topLeft,
-                  padding: EdgeInsets.fromLTRB(17, 10, 0, 0),
-                  child: StarDisplayWidget(
-                    value: review.starScore,
-                    filledStar: Icon(Icons.star, color: Colors.amber, size: 20),
-                    unfilledStar: Icon(Icons.star_border, color: Colors.grey),
-                  ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 7,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(15, 5, 15, 0),
-                        child: Text(
-                          review.reviewsContent,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 15,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  alignment: Alignment.bottomRight,
-                  padding: EdgeInsets.fromLTRB(0, 10, 15, 5),
-                  child: Text(
-                    review.createdAt,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
+                _displayStarScore(review.starScore),
+                _displayReviewsContent(review.reviewsContent),
+                _displayDateTime(review.createdAt),
               ],
             ),
           ),
